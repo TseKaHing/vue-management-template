465a8e04d443ad9e85e45ce70f97d82a97c7182a
jazzyXie <1243756230@qq.com>
2019-05-01 19:29:20 +0800

    vue-cli3.0配置

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import config from './config'
 
 Vue.config.productionTip = false
 
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,20 @@
+const BASE_URL = process.env.NODE_ENV === 'production' ? '/iview-admin': '/'
+const path = require('path')
+
+const resolve = dir => path.join(__dirname, dir) 
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  baseUrl:BASE_URL,
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', resolve('src'))
+      .set('_c', resolve('src/components'))
+  },
+  // 打包时不生成.map文件，可减少打包时的体积，加快打包速度 
+  productionSourceMap: false,
+  devServer: {
+    // 需要代理的URL, 任何位置请求都代理到这个URL
+    proxy: 'http://localhost:4000'
+  }
+
 }
